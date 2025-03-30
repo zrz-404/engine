9abf1ab626e65d8893bdefac1c3011642faa17c2
zrz-404 <jproseiro@gmail.com>
2025-03-30 05:32:59 +0200

    first window utils implemented

---
+++ src/core/window.h
@@ -0,0 +1,53 @@
+#pragma once //keyword similar to ifndef, def combo
+
+#include <string>
+#include <functional>
+#include <GLFW/glfw3.h>
+
+namespace Engine {
+
+struct WindowProps {
+    std::string Title;
+    unsigned int Width;
+    unsigned int Height;
+
+    WindowProps(const std::string& title = "zrz",
+                unsigned int width = 1280,
+                unsigned int height = 720)
+        : Title(title), Width(width), Height(height) {}
+};
+
+//window class providing a cross-platform window abstraction
+class Window {
+    public:
+        using EventCallbackFn = std::function<void(/*Event& event*/)>;
+
+        Window(const WindowProps& props);
+        ~Window();
+
+        void OnUpdate();
+
+        unsigned int GetWidth() const { return m_Data.Width; }
+        unsigned int GetHeight() const { return m_Data.Height; }
+
+        // Window attributes
+        void SetEventCallback(const EventCallbackfn& callback) {m_Data.EventCallback = callback; }
+        void SetVsync(bool enabled);
+        void IsVsync() const;
+
+        void* GetNativeWindow() const { return m_Window; }
+
+    private:
+        GLFWwindow* m_Window;
+
+        struct WindowData {
+            std::string Title;
+            unsigned int Width, Height;
+            bool VSync;
+
+            EventCallbackFn EventCallback;
+        };
+        WindowData m_Data;
+};
+
+} // namespace engine
